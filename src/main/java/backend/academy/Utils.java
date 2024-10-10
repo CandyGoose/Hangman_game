a18f096602cec18c3370b40537b57d20017dce19
Vera Kasianenko <112972833+CandyGoose@users.noreply.github.com>
2024-10-11 01:32:03 +0300

    style: исправлены ошибки Checkstyle
    - исправлены ошибки Checkstyle (приватные конструкторы и именование констант)

--- src/main/java/backend/academy/Utils.java
+++ src/main/java/backend/academy/Utils.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Random;
 
 public class Utils {
-    private static final Random random = new Random();
+
+    private Utils() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    private static final Random RANDOM = new Random();
 
     public static <T> T getRandomValue(List<T> values) {
         if (values == null || values.isEmpty()) {
             throw new IllegalArgumentException("Список не должен быть пустым");
         }
-        return values.get(random.nextInt(values.size()));
+        return values.get(RANDOM.nextInt(values.size()));
     }
 }
